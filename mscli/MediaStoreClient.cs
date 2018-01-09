336398e2eff65a4dfddd3a78a8217b348a4525dc
jpompeii <lksm6@yahoo.com>
2018-01-08 20:46:19 -0500

    added initial command line test program

---
+++ mscli/MediaStoreClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaStore.Client
+{
+    public class MediaStoreClient
+    {
+        private string _baseUrl;
+        private HttpClient _client;
+
+        // usage: mscli baseAddr library file
+
+        public MediaStoreClient(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(baseUrl);
+        }
+
+        public string GetLibraryAsync(string libName)
+        {
+            var result = _client.GetAsync("api/mediastore/libraries").Result;
+            var json = result.Content.ReadAsStringAsync().Result;
+            var libraries = JsonConvert.DeserializeObject<List<LibraryValue>>(responseBody);
+            
+        }
+
+        public async Task<string> AddFileAsync(string filePath, string libraryId)
+        {
+            byte[] fileBytes = File.ReadAllBytes(filePath);
+            ByteArrayContent bytes = new ByteArrayContent(fileBytes);
+            MultipartFormDataContent multiContent = new MultipartFormDataContent();
+            multiContent.Add(bytes, "file", Path.GetFileName(filePath));
+            var result = await _client.PostAsync($"api/mediastore/libraries/{libraryId}/asset", multiContent);
+            return result.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
---
+++ mscli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MediaStore.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MediaStore CLI");
+
+        }
+    }
+}
