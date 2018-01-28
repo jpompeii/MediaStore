1896f54e613e4a90c135d145212e71497a5701d8
jpompeii <lksm6@yahoo.com>
2018-01-27 22:10:51 -0500

    added demo program skeleton for s3

---
+++ s3cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace s3cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
